2e47f8fa005f2f07ca62636fc722a27604459525
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-08-13 22:08:31 +0500

    Escape Sequences

---
+++ escapeSequences.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+int main()
+{
+    // Escape Sequences
+    /*
+    1) \n
+    2) \t
+    3) \r
+    4) \a
+    5) \\
+    6) \'
+    7) \"
+    */
+    cout << """\n : Used for: Newline -> Point the cursor in the beginning of the new line"""<<endl;
+    cout << """\t: Usef for: Horizontal tab"""<<endl;
+    return 0;
+}
+
